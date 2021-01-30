bea47b1b1e3e22d3648035ce00d3302112cf6f38
Elena <2734.89@mail.ru>
2021-01-30 18:29:28 +0300

    primary routing has been set

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,16 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home Page Test</router-link> |
-    <router-link to="/about">About</router-link>
+    <h1>Welcome to Nonogram!</h1>
+    <router-link to="/">Home</router-link> 
+    <router-link to="/game">Start game</router-link>
   </div>
   <router-view/>
 </template>
 
 <style lang="scss">
-$image-path: '~@/../mdb/mdbvue/img';
-@import '~@/../mdb/mdbvue/scss/mdb-free.scss';
-
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+* {
+  font: 15pt sans-serif;
+  margin: 0;
+  padding: 0;
 }
 </style>
---
+++ src/views/Game.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="game">
+    <h2>There will be a game</h2>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Field from '@/components/Field.vue'
+
+export default {
+  name: 'Field',
+  components: {
+    Field
+  }
+}
+</script>
