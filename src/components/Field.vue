92737f2a96c6eb15f03949ec06d5271597dba3e0
Elena <2734.89@mail.ru>
2021-02-04 22:53:14 +0300

    adaptive cells are styled

--- src/components/Field.vue
+++ src/components/Field.vue
@@ -2,8 +2,9 @@
   <div class="container">
     <div class="container-fluid">
       <div v-for="row in content.length" :key="row" class="row">
-        <div v-for="col in content.length" :key="col" class="cell">
-          <h2>Cell {{row.toString() + " " + col.toString() }}</h2>
+        <div v-for="col in content.length" :key="col" class="cell"
+          v-bind:style="sellSize">
+          Cell {{row.toString() + " " + col.toString() }}
         </div>
       </div>
     </div>
@@ -17,10 +18,35 @@ export default {
     content: {
       type: Array
     }
+  },
+  computed: {
+    sellSize: function() {
+      return "width: " + 100/this.content.length + "%;";
+    }
   }
 };
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+
+.container, .cell {
+  border: 3px solid black;
+}
+
+.container {
+  width: 100%;
+  aspect-ratio: 1/1;
+}
+
+.cell {
+  aspect-ratio: 1/1;
+  background: grey;
+}
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 
 </style>
